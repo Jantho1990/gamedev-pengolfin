a37a18bc5ec3c7d6fce20d4c5631b2422977fe64
Joshua Anthony <jantho1990@gmail.com>
2018-09-24 21:59:16 -0500

    added matter and quick render demo

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import pop from '../pop'
 const { Game, entity, math, Text } = pop
+import KeyControls from '../pop/controls/KeyControls';
+import MouseControls from '../pop/controls/MouseControls';
+import GameScreen from './GameScreen'
 
-const game = new Game(640, 480)
-const { scene, w, h, } = game
-
-const style = {
-  fill: 'hsl(320, 20%, 20%)',
-  font: '40pt "Arial", monospace'
+const game = new Game(800, 400)
+const controls = {
+  keys: new KeyControls(),
+  mouse: new MouseControls(game.renderer.view)
 }
-
-const demoText1 = new Text('Gamedev', style)
-demoText1.pos = { x: w / 4, y: 125 }
-
-const demoText2 = new Text('Boilerplate', style)
-demoText2.pos = { x: w / 3, y: 175 }
-
-scene.add(demoText1)
-scene.add(demoText2)
-
+function playHole() {
+  game.scene = new GameScreen(game, controls, playHole)
+}
+playHole()
 game.run()
